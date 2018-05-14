f935c5d158fe1debf8c48990ef3963e456ce64e3
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2018-05-15 11:39:39 +1200

    Add in stub of statewment_er.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,32 @@
+{-# Language TemplateHaskell, QuasiQuotes, FlexibleContexts, DeriveDataTypeable, Haskell2010, OverloadedStrings, Arrows, NoMonomorphismRestriction, LambdaCase #-}
+
 module Lib
     ( someFunc
     ) where
 
 import Text.XML.HXT.Core
+import Data.Text (Text, pack)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
+data StatementCategory = PrescriptiveStatement | ConstituiveStatement deriving (Show, Eq)
 
+data Statement_ed = Statement_ed { statement_category :: StatementCategory, strength, key, formula_frag_id :: Text}
+
+-- statement_er :: XmlTree -> [Statement_ed]
+-- statement_er doc = let statement_s = ((multi (isElem >>> ((hasName "PrescriptiveStatement") <+> (hasName "ConstituiveStatement")))) doc) in
+--                        map (\stmt -> Statement_ed (statement_type stmt) "" "" "") statement_s
+
+statement_er doc = proc doc -> do
+                     returnA -< Statement_ed {
+                       statement_category = PrescriptiveStatement,
+                       strength = "defeasible",
+                       key = "",
+                       formula_frag_id = "0" }
+
+statement_type :: Text -> StatementCategory
+statement_type = \case
+                           "PrescriptiveStatement" -> PrescriptiveStatement
+                           "ConstituiveStatement"  -> ConstituiveStatement
+                           _                       -> error "Not a statement"
