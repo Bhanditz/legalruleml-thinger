7dee8c8add812a975161a0d361097048163e7d6d
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2018-07-18 14:55:51 +1200

    Add in init command line option; refactor options parsing.

--- app/Main.hs
+++ app/Main.hs
@@ -11,46 +11,61 @@ import Control.Applicative
 import Data.List
 import Data.Text
 
-data AppOptions = AppOptions {
-  fileName :: Text,
-  act      :: Command
-}
-
-data Command = PrintOut | PopulateDatabase String
+data Command = PrintOut Text | PopulateDatabase Text Text | InitDatabase Text
 
 withInfo :: Parser a -> String -> ParserInfo a
 withInfo opts desc = info (helper <*> opts) $ progDesc desc 
 
-appoptions :: OA.Parser AppOptions
+appoptions :: OA.Parser Command
 appoptions = subparser $
         command "print" (printCmd `withInfo` "Print out the structured information extracted from a LegalRuleML file") <>
-        command "db"    (dbCmd    `withInfo` "Populate a database with information extracted from a LegalRuleML file")
+        command "db"    (dbCmd    `withInfo` "Populate a database with information extracted from a LegalRuleML file") <>
+        command "init"  (initCmd  `withInfo` "Initialise a database with the tables needed to store LegalRuleML data.")
 
 fileBit :: OA.Parser Text
 fileBit = runA $ proc () -> do
             file <- asA (argument str (metavar "FILE" <> help "File to parse")) -< ()
             returnA -< pack file
 
-printCmd :: OA.Parser AppOptions
+dbBit :: OA.Parser Text
+dbBit = runA $ proc () -> do
+            db <- asA (argument str (metavar "DBNAME" <> help "Database to connect to")) -< ()
+            returnA -< pack db
+
+printCmd :: OA.Parser Command
 printCmd =  runA $ proc () -> do
              fileName <- asA fileBit -< ()
-             returnA -< AppOptions { fileName = fileName, act = PrintOut }
+             returnA -< PrintOut fileName
 
-dbCmd :: OA.Parser AppOptions
-dbCmd = AppOptions
+dbCmd :: OA.Parser Command
+dbCmd = PopulateDatabase
           <$> fileBit
-          <*> (PopulateDatabase <$> argument str
-              ( metavar "DBNAME"
-             <> help "Database to connect to" ))
+          <*> dbBit
+
+initCmd :: OA.Parser Command
+initCmd = InitDatabase
+           <$> dbBit
 
 
-real_main :: AppOptions -> IO ()
+getData :: Text -> IO [Statement_ed]
+getData fileName = do
+    runX (readDocument [withRemoveWS yes] (unpack fileName) >>> getChildren >>> isElem >>> hasName "lrml:LegalRuleML" >>> all_statement_er)
+
+populateDb :: [Statement_ed] -> Text -> IO ()
+populateDb stuff dbName = do
+    print stuff
+
+real_main :: Command -> IO ()
 real_main options =
     do
-      stmts <- runX (readDocument [withRemoveWS yes] (unpack (fileName options)) >>> getChildren >>> isElem >>> hasName "lrml:LegalRuleML" >>> all_statement_er)
-      case (act options) of
-        PrintOut            -> print stmts
-        PopulateDatabase db -> print "..."
+      case options of
+        InitDatabase     db      -> print db
+        PrintOut         file    -> do
+            stuff <- getData file
+            print stuff
+        PopulateDatabase file db -> do
+            stuff <- getData file
+            populateDb stuff db
 
 main :: IO ()
 main = execParser opts >>= real_main
